b2a80944595cddc90c482b335416368c16344553
MugureNjunge <maureennjunge22@gmail.com>
2022-03-18 18:30:50 +0300

    add my form for checkboxes and radio buttons

---
+++ js/scripts.js
@@ -0,0 +1,14 @@
+var pizza = ("size", "crust", "toppings"){
+  var size = ("small", "medium", "large");
+  var crust = ("crispy", "stuffed", "gluten-free", "classic-thin", "classic-thick");
+  var topings = ("Sausage and Mushrooms " , "Pepperoni and Bacon" ,
+  "Ham and Pineapple" ,"Sausage and Pepper", "Ham and Mushroom", "Pepperoni and Pepper",
+  "Cheese and Basil leaves","Mushroom and pepperoni","Macon and Mushroom");
+}
+
+
+
+
+(document).ready(function(){
+
+}

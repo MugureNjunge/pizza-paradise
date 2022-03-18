844c3b0feb99babc6048643309097b9cd24fd980
MugureNjunge <maureennjunge22@gmail.com>
2022-03-18 19:27:35 +0300

    add prices and order button

--- js/scripts.js
+++ js/scripts.js
@@ -1,14 +1,7 @@
-var pizza = ("size", "crust", "toppings"){
-  var size = ("small", "medium", "large");
-  var crust = ("crispy", "stuffed", "gluten-free", "classic-thin", "classic-thick");
-  var topings = ("Sausage and Mushrooms " , "Pepperoni and Bacon" ,
-  "Ham and Pineapple" ,"Sausage and Pepper", "Ham and Mushroom", "Pepperoni and Pepper",
-  "Cheese and Basil leaves","Mushroom and pepperoni","Macon and Mushroom");
-}
 
 
 
 
-(document).ready(function(){
+$(document).ready(function(){
 
 }

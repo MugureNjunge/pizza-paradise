c3c6ea05aecb4a3201a0b32fd90f3e99e9efb369
MugureNjunge <maureennjunge22@gmail.com>
2022-03-21 07:36:40 +0300

    add the order functionality

--- js/scripts.js
+++ js/scripts.js
@@ -1,40 +1,64 @@
-function Pizza() {
-  let size = document.querySelector('input[name="size"]:checked').value
-  let crust = document.querySelector('input[name="crust"]:checked').value
-  let toppings = [];
-  let checkboxes = document.getElementByName('toppings[]');
-  for (let i = 0; i < checkboxes.length; i++) {
-    if (checkboxes[i].checked) {
-      toppings.push(checkboxes[i].value);
-    }
-  }
+// function Pizza() {
+//   let size = document.querySelector('input[name="size"]:checked').value
+//   let crust = document.querySelector('input[name="crust"]:checked').value
+//   let toppings = [];
+//   let checkboxes = document.getElementByName('toppings[]');
+//   for (let i = 0; i < checkboxes.length; i++) {
+//     if (checkboxes[i].checked) {
+//       toppings.push(checkboxes[i].value);
 
-}
-
-// let pizzaTotal = ("size", "crust", "toppings"){
-//   let sizes = ("small", "medium", "large");
-//   let sizePrice = (300, 500, 700);
-//   let crust = ("crispy", "gluten-free", "classic-thin", "classic-thick");
-//   let crustPrice = (150, 150, 150, 150);
-//   let toppings = ("Sausage and Mushrooms ", "Pepperoni and Bacon",
-//   "Ham and Pineapple", "Sausage and Pepper");
-//   let toppingsPrice = (150, 150, 150,150,);
-
-
-// let pizzaTotal = (size + crust + (toppings).checked + (delivery).checked).value;
-// return true ;
-// alert ("Your total is 'pizzaTotal'");
+//     }
+//   }
 
 // }
 
-if (size.value == "large" && crust.value=="crispy" && toppings.value=="Sausage and Mushrooms"){
-  let total= 700 + 150 +150;
-}
+
+// function Pizza() {
+//   let size = document.getElementById("size").value 
+//   let crust = document.getElementById("crust").value
+//   let toppings = [];
+//   let checkboxes = document.getElementById("toppings").checked;
+//     (let i = 0; i < checkboxes.length; i++) {
+//     if (checkboxes[i].checked) {
+//       toppings.push(checkboxes[i].value);
+
+//     }
+//   }
+
+// }
+// let pizzaTotal = ("size", "crust", "toppings"){
+//   let pizzaSizes = ("small", "medium", "large");
+// let sizePrice = (300, 500, 700);
+// let crust = ("crispy", "gluten-free", "classic-thin", "classic-thick");
+// let crustPrice = (150, 150, 150, 150);
+// let toppings = ("Sausage and Mushrooms ", "Pepperoni and Bacon",
+//   "Ham and Pineapple", "Sausage and Pepper");
+// let toppingsPrice = (150, 150, 150, 150 );
+// }
 
 
+// let pizzaTotal = (size + crust + (toppings).checked + (delivery).checked).value;
+// return true;
+// alert("Your total is 'pizzaTotal'");
+
+// }
+
+// if (size.value == "large" && crust.value == "crispy" && toppings.value == "Sausage and Mushrooms") {
+//   let total = 700 + 150 + 150;
+//   alert ("Your total price is pizzaTotal")
+
+// if (size.value == "large" && crust.value == "crispy" && toppings.value == "Sausage and Mushrooms") {
+//   let total = 700 + 150 + 150 is pizzaTotal;
+//   alert ("Your total price is pizzaTotal")
 
 
+// if (size.value == "large" && crust.value == "crispy" && toppings.value == "Sausage and Mushrooms") {
+//   let total = 700 + 150 + 150;
+//   alert ("Your total price is pizzaTotal")
 
-$(document).ready(function () {
+// if (size.value == "large" && crust.value == "crispy" && toppings.value == "Sausage and Mushrooms") {
+//         let total = 700 + 150 + 150;
 
+formSubmit(){
+  letpizzaSize = 
 }

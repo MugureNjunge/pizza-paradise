164dcd3f8755dea5307e61c00aba15ed044fa5fb
MugureNjunge <maureennjunge22@gmail.com>
2022-03-21 14:42:50 +0300

    clear the commented code

--- scripts.js
+++ scripts.js
@@ -5,9 +5,11 @@ function myFunction() {
   let yummy = document.getElementById("topping");
   let pizzaSize = document.getElementById("size");
   let pizzaCrust =document.getElementById("crust");
+  let transport= document.getElementById("delivery");
   let yummyPrice =[150, 150, 150, 150];
   let crustPrice = [200,200,200];
-  let delivery =[200]
+  let transport =[200];
+ 
 
   // mushrooms
 
@@ -18,49 +20,49 @@ function myFunction() {
   }
 
   if (pizzaSize.value=="large" && yummy.value=="mushrooms" && pizzaCrust.value == "gluten-free"){
-    alert("You placed an order for a " + pizzaSize.value + " pizza with " + yummy.value + " toppings and a " + pizzaCrust.value + " crust" );
+    alert("You placed an order for a " + pizzaSize.value + " pizza with " + yummy.value + " toppings and a " + pizzaCrust.value + " crust. Press ok. to see your total cost!" );
   alert(sizePrice[2] + yummyPrice[0] + crustPrice[1]);
 
   }
 
   if (pizzaSize.value=="large" && yummy.value=="mushrooms" && pizzaCrust.value == "thick-crust"){
-    alert("You placed an order for a " + pizzaSize.value + " pizza with " + yummy.value + " toppings and a " + pizzaCrust.value + " crust" );
+    alert("You placed an order for a " + pizzaSize.value + " pizza with " + yummy.value + " toppings and a " + pizzaCrust.value + " crust. Press ok. to see your total cost!" );
   alert(sizePrice[2] + yummyPrice[0] + crustPrice[2]);
 
   }
 
   if (pizzaSize.value=="medium" && yummy.value=="mushrooms" && pizzaCrust.value == "crispy"){
-    alert("You placed an order for a " + pizzaSize.value + " pizza with " + yummy.value + " toppings and a " + pizzaCrust.value + " crust" );
+    alert("You placed an order for a " + pizzaSize.value + " pizza with " + yummy.value + " toppings and a " + pizzaCrust.value + " crust. Press ok. to see your total cost!" );
   alert(sizePrice[1] + yummyPrice[0] + crustPrice[0]);
 
   }
 
   if (pizzaSize.value=="medium" && yummy.value=="mushrooms" && pizzaCrust.value == "gluten-free"){
-    alert("You placed an order for a " + pizzaSize.value + " pizza with  " + yummy.value + " toppings and a " + pizzaCrust.value + " crust" );
+    alert("You placed an order for a " + pizzaSize.value + " pizza with  " + yummy.value + " toppings and a " + pizzaCrust.value + " crust. Press ok. to see your total cost!" );
   alert(sizePrice[1] + yummyPrice[0] + crustPrice[1]);
 
   }
 
   if (pizzaSize.value=="medium" && yummy.value=="mushrooms" && pizzaCrust.value == "thick-crust"){
-    alert("You placed an order for a " + pizzaSize.value + " pizza with  " + yummy.value + " toppings and a " + pizzaCrust.value + " crust" );
+    alert("You placed an order for a " + pizzaSize.value + " pizza with  " + yummy.value + " toppings and a " + pizzaCrust.value + " crust. Press ok. to see your total cost!" );
   alert(sizePrice[1] + yummyPrice[0] + crustPrice[2]);
 
   }
 
   if (pizzaSize.value=="small" && yummy.value=="mushrooms" && pizzaCrust.value == "crispy"){
-    alert("You placed an order for a " + pizzaSize.value + " pizza with  " + yummy.value + " toppings and a " + pizzaCrust.value + " crust" );
+    alert("You placed an order for a " + pizzaSize.value + " pizza with  " + yummy.value + " toppings and a " + pizzaCrust.value + " crust. Press ok. to see your total cost!" );
   alert(sizePrice[0] + yummyPrice[0] + crustPrice[0]);
 
   }
 
   if (pizzaSize.value=="small" && yummy.value=="mushrooms" && pizzaCrust.value == "gluten-free"){
-    alert("You placed an order for a " + pizzaSize.value + " pizza with  " + yummy.value + " toppings and a " + pizzaCrust.value + " crust" );
+    alert("You placed an order for a " + pizzaSize.value + " pizza with  " + yummy.value + " toppings and a " + pizzaCrust.value + " crust. Press ok. to see your total cost!" );
   alert(sizePrice[0] + yummyPrice[0] + crustPrice[1]);
 
   }
 
   if (pizzaSize.value=="small" && yummy.value=="mushrooms" && pizzaCrust.value == "thick-crust"){
-    alert("You placed an order for a " + pizzaSize.value + " pizza with  " + yummy.value + " toppings and a " + pizzaCrust.value + " crust" );
+    alert("You placed an order for a " + pizzaSize.value + " pizza with  " + yummy.value + " toppings and a " + pizzaCrust.value + " crust. Press ok. to see your total cost!" );
   alert(sizePrice[0] + yummyPrice[0] + crustPrice[2]);
 
   }
@@ -68,7 +70,7 @@ function myFunction() {
   // pineapple
 
   if (pizzaSize.value=="large" && yummy.value=="pineapple" && pizzaCrust.value == "crispy"){
-    alert("You placed an order for a " + pizzaSize.value + " pizza with " + yummy.value + " toppings and a " + pizzaCrust.value + " crust" );
+    alert("You placed an order for a " + pizzaSize.value + " pizza with " + yummy.value + " toppings and a " + pizzaCrust.value + " crust. Press ok. to see your total cost!" );
   alert(sizePrice[2] + yummyPrice[1] + crustPrice[0]);
 
   }
@@ -80,98 +82,98 @@ function myFunction() {
   }
 
   if (pizzaSize.value=="large" && yummy.value=="pineapple" && pizzaCrust.value == "thick-crust"){
-    alert("You placed an order for a " + pizzaSize.value + " pizza with " + yummy.value + " toppings and a " + pizzaCrust.value + " crust" );
+    alert("You placed an order for a " + pizzaSize.value + " pizza with " + yummy.value + " toppings and a " + pizzaCrust.value + " crust. Press ok. to see your total cost!" );
   alert(sizePrice[2] + yummyPrice[1] + crustPrice[2]);
 
   }
 
   if (pizzaSize.value=="medium" && yummy.value=="pineapple" && pizzaCrust.value == "crispy"){
-    alert("You placed an order for a " + pizzaSize.value + " pizza with  " + yummy.value + " toppings and a " + pizzaCrust.value + " crust" );
+    alert("You placed an order for a " + pizzaSize.value + " pizza with  " + yummy.value + " toppings and a " + pizzaCrust.value + " crust. Press ok. to see your total cost!" );
   alert(sizePrice[1] + yummyPrice[1] + crustPrice[0]);
 
   }
 
   if (pizzaSize.value=="medium" && yummy.value=="pineapple" && pizzaCrust.value == "gluten-free"){
-    alert("You placed an order for a " + pizzaSize.value + " pizza with  " + yummy.value + " toppings and a " + pizzaCrust.value + " crust" );
+    alert("You placed an order for a " + pizzaSize.value + " pizza with  " + yummy.value + " toppings and a " + pizzaCrust.value + " crust. Press ok. to see your total cost!" );
   alert(sizePrice[1] + yummyPrice[1] + crustPrice[1]);
 
   }
 
   if (pizzaSize.value=="medium" && yummy.value=="pineapple" && pizzaCrust.value == "thick-crust"){
-    alert("You placed an order for a " + pizzaSize.value + " pizza with  " + yummy.value + " toppings and a " + pizzaCrust.value + " crust" );
+    alert("You placed an order for a " + pizzaSize.value + " pizza with  " + yummy.value + " toppings and a " + pizzaCrust.value + " crust. Press ok. to see your total cost!" );
   alert(sizePrice[1] + yummyPrice[1] + crustPrice[2]);
 
   }
 
   if (pizzaSize.value=="small" && yummy.value=="pineapple" && pizzaCrust.value == "crispy"){
-    alert("You placed an order for a " + pizzaSize.value + " pizza with " + yummy.value + " toppings and a " + pizzaCrust.value + " crust" );
+    alert("You placed an order for a " + pizzaSize.value + " pizza with " + yummy.value + " toppings and a " + pizzaCrust.value + " crust. Press ok. to see your total cost!" );
   alert(sizePrice[0] + yummyPrice[1] + crustPrice[0]);
 
   }
 
   if (pizzaSize.value=="small" && yummy.value=="pineapple" && pizzaCrust.value == "gluten-free"){
-    alert("You placed an order for a " + pizzaSize.value + " pizza with " + yummy.value + " toppings and a " + pizzaCrust.value + " crust" );
+    alert("You placed an order for a " + pizzaSize.value + " pizza with " + yummy.value + " toppings and a " + pizzaCrust.value + " crust. Press ok. to see your total cost!" );
   alert(sizePrice[0] + yummyPrice[1] + crustPrice[1]);
 
   }
 
   if (pizzaSize.value=="small" && yummy.value=="pineapple" && pizzaCrust.value == "thick-crust"){
-    alert("You placed an order for a " + pizzaSize.value + " pizza with  " + yummy.value + " toppings and a " + pizzaCrust.value + " crust" );
+    alert("You placed an order for a " + pizzaSize.value + " pizza with  " + yummy.value + " toppings and a " + pizzaCrust.value + " crust. Press ok. to see your total cost!" );
   alert(sizePrice[0] + yummyPrice[1] + crustPrice[2]);
 
   }
 
   //  ham
   if (pizzaSize.value=="large" && yummy.value=="ham" && pizzaCrust.value == "crispy"){
-    alert("You placed an order for a " + pizzaSize.value + " pizza with  " + yummy.value + " toppings and a " + pizzaCrust.value + " crust" );
+    alert("You placed an order for a " + pizzaSize.value + " pizza with  " + yummy.value + " toppings and a " + pizzaCrust.value + " crust. Press ok. to see your total cost!" );
   alert(sizePrice[2] + yummyPrice[2] + crustPrice[0]);
 
   }
 
   if (pizzaSize.value=="large" && yummy.value=="ham" && pizzaCrust.value == "gluten-free"){
-    alert("You placed an order for a " + pizzaSize.value + " pizza with " + yummy.value + " toppings and a " + pizzaCrust.value + " crust" );
+    alert("You placed an order for a " + pizzaSize.value + " pizza with " + yummy.value + " toppings and a " + pizzaCrust.value + " crust. Press ok. to see your total cost!" );
   alert(sizePrice[2] + yummyPrice[2] + crustPrice[1]);
 
   }
 
   if (pizzaSize.value=="large" && yummy.value=="ham" && pizzaCrust.value == "thick-crust"){
-    alert("You placed an order for a " + pizzaSize.value + " pizza with " + yummy.value + " toppings and a " + pizzaCrust.value + " crust" );
+    alert("You placed an order for a " + pizzaSize.value + " pizza with " + yummy.value + " toppings and a " + pizzaCrust.value + " crust. Press ok. to see your total cost!" );
   alert(sizePrice[2] + yummyPrice[2] + crustPrice[2]);
 
   }
 
   if (pizzaSize.value=="medium" && yummy.value=="ham" && pizzaCrust.value == "crispy"){
-    alert("You placed an order for a " + pizzaSize.value + " pizza with " + yummy.value + " toppings and a " + pizzaCrust.value + " crust" );
+    alert("You placed an order for a " + pizzaSize.value + " pizza with " + yummy.value + " toppings and a " + pizzaCrust.value + " crust. Press ok. to see your total cost!" );
   alert(sizePrice[1] + yummyPrice[2] + crustPrice[0]);
 
   }
 
   if (pizzaSize.value=="medium" && yummy.value=="ham" && pizzaCrust.value == "gluten-free"){
-    alert("You placed an order for a " + pizzaSize.value + " pizza with  " + yummy.value + " topping and a " + pizzaCrust.value + " crust" );
+    alert("You placed an order for a " + pizzaSize.value + " pizza with  " + yummy.value + " topping and a " + pizzaCrust.value + " crust. Press ok. to see your total cost!" );
   alert(sizePrice[1] + yummyPrice[2] + crustPrice[1]);
 
   }
 
   if (pizzaSize.value=="medium" && yummy.value=="ham" && pizzaCrust.value == "thick-crust"){
-    alert("You placed an order for a " + pizzaSize.value + " pizza with " + yummy.value + " toppings and a " + pizzaCrust.value + " crust" );
+    alert("You placed an order for a " + pizzaSize.value + " pizza with " + yummy.value + " toppings and a " + pizzaCrust.value + " crust. Press ok. to see your total cost!" );
   alert(sizePrice[1] + yummyPrice[2] + crustPrice[2]);
 
   }
 
   if (pizzaSize.value=="small" && yummy.value=="ham" && pizzaCrust.value == "crispy"){
-    alert("You placed an order for a " + pizzaSize.value + " pizza with " + yummy.value + " toppings and a " + pizzaCrust.value + " crust" );
+    alert("You placed an order for a " + pizzaSize.value + " pizza with " + yummy.value + " toppings and a " + pizzaCrust.value + " crust. Press ok. to see your total cost!" );
   alert(sizePrice[0] + yummyPrice[2] + crustPrice[0]);
 
   }
 
   if (pizzaSize.value=="small" && yummy.value=="ham" && pizzaCrust.value == "gluten-free"){
-    alert("You placed an order for a " + pizzaSize.value + " pizza with " + yummy.value + " toppings and a " + pizzaCrust.value + " crust" );
+    alert("You placed an order for a " + pizzaSize.value + " pizza with " + yummy.value + " toppings and a " + pizzaCrust.value + " crust. Press ok. to see your total cost!" );
   alert(sizePrice[0] + yummyPrice[2] + crustPrice[1]);
 
   }
 
   if (pizzaSize.value=="small" && yummy.value=="ham" && pizzaCrust.value == "thick-crust"){
-    alert("You placed an order for a " + pizzaSize.value + " pizza with " + yummy.value + " toppings and a " + pizzaCrust.value + " crust" );
+    alert("You placed an order for a " + pizzaSize.value + " pizza with " + yummy.value + " toppings and a " + pizzaCrust.value + " crust. Press ok. to see your total cost!" );
   alert(sizePrice[0] + yummyPrice[2] + crustPrice[2]);
 
   }
@@ -185,49 +187,49 @@ function myFunction() {
   }
 
   if (pizzaSize.value=="large" && yummy.value=="pepperoni" && pizzaCrust.value == "gluten-free"){
-    alert("You placed an order for a " + pizzaSize.value + " pizza with " + yummy.value + " toppings and a " + pizzaCrust.value + " crust" );
+    alert("You placed an order for a " + pizzaSize.value + " pizza with " + yummy.value + " toppings and a " + pizzaCrust.value + " crust. Press ok. to see your total cost!" );
   alert(sizePrice[2] + yummyPrice[3] + crustPrice[1]);
 
   }
 
   if (pizzaSize.value=="large" && yummy.value=="pepperoni" && pizzaCrust.value == "thick-crust"){
-    alert("You placed an order for a " + pizzaSize.value + " pizza with " + yummy.value + " toppings and a " + pizzaCrust.value + " crust" );
+    alert("You placed an order for a " + pizzaSize.value + " pizza with " + yummy.value + " toppings and a " + pizzaCrust.value + " crust. Press ok. to see your total cost!" );
   alert(sizePrice[2] + yummyPrice[3] + crustPrice[2]);
 
   }
 
   if (pizzaSize.value=="medium" && yummy.value=="pepperoni" && pizzaCrust.value == "crispy"){
-    alert("You placed an order for a " + pizzaSize.value + " pizza with " + yummy.value + " toppings and a " + pizzaCrust.value + " crust" );
+    alert("You placed an order for a " + pizzaSize.value + " pizza with " + yummy.value + " toppings and a " + pizzaCrust.value + " crust. Press ok. to see your total cost!" );
   alert(sizePrice[1] + yummyPrice[3] + crustPrice[0]);
 
   }
 
   if (pizzaSize.value=="medium" && yummy.value=="pepperoni" && pizzaCrust.value == "gluten-free"){
-    alert("You placed an order for a " + pizzaSize.value + " pizza with " + yummy.value + " toppings and a " + pizzaCrust.value + " crust" );
+    alert("You placed an order for a " + pizzaSize.value + " pizza with " + yummy.value + " toppings and a " + pizzaCrust.value + " crust. Press ok. to see your total cost!" );
   alert(sizePrice[1] + yummyPrice[3] + crustPrice[1]);
 
   }
 
   if (pizzaSize.value=="medium" && yummy.value=="pepperoni" && pizzaCrust.value == "thick-crust"){
-    alert("You placed an order for a " + pizzaSize.value + " pizza with " + yummy.value + " toppings and a " + pizzaCrust.value + " crust" );
+    alert("You placed an order for a " + pizzaSize.value + " pizza with " + yummy.value + " toppings and a " + pizzaCrust.value + " crust. Press ok. to see your total cost!" );
   alert(sizePrice[1] + yummyPrice[3] + crustPrice[2]);
 
   }
 
   if (pizzaSize.value=="small" && yummy.value=="pepperoni" && pizzaCrust.value == "crispy"){
-    alert("You placed an order for a " + pizzaSize.value + " pizza with " + yummy.value + " toppings and a " + pizzaCrust.value + " crust" );
+    alert("You placed an order for a " + pizzaSize.value + " pizza with " + yummy.value + " toppings and a " + pizzaCrust.value + " crust. Press ok. to see your total cost!" );
   alert(sizePrice[0] + yummyPrice[3] + crustPrice[0]);
 
   }
 
   if (pizzaSize.value=="small" && yummy.value=="ham" && pizzaCrust.value == "gluten-free"){
-    alert("You placed an order for a " + pizzaSize.value + " pizza with " + yummy.value + " toppings and a " + pizzaCrust.value + " crust" );
+    alert("You placed an order for a " + pizzaSize.value + " pizza with " + yummy.value + " toppings and a " + pizzaCrust.value + " crust. Press ok. to see your total cost!" );
   alert(sizePrice[0] + yummyPrice[2] + crustPrice[1]);
 
   }
 
   if (pizzaSize.value=="small" && yummy.value=="pepperoni" && pizzaCrust.value == "thick-crust"){
-    alert("You placed an order for a " + pizzaSize.value + " pizza with " + yummy.value + " toppings and a " + pizzaCrust.value + " crust" );
+    alert("You placed an order for a " + pizzaSize.value + " pizza with " + yummy.value + " toppings and a " + pizzaCrust.value + " crust. Press ok. to see your total cost!" );
   alert(sizePrice[0] + yummyPrice[3] + crustPrice[2]);
 
   }

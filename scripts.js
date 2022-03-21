ac67fea62ed080da5898367e33de22a1b657936c
MugureNjunge <maureennjunge22@gmail.com>
2022-03-21 13:46:18 +0300

    add the delivery fee function

--- scripts.js
+++ scripts.js
@@ -5,204 +5,179 @@ function myFunction() {
   let yummy = document.getElementById("topping");
   let pizzaSize = document.getElementById("size");
   let pizzaCrust =document.getElementById("crust");
-  let yummyPrice =[100, 150, 200, 250];
-  let crustPrice = [200,250,300];
+  let yummyPrice =[150, 150, 150, 150];
+  let crustPrice = [200,200,200];
+
+  // mushrooms
 
   if (pizzaSize.value=="large" && yummy.value=="mushrooms" && pizzaCrust.value == "crispy"){
     alert("you placed an order for a " + pizzaSize.value + " pizza with a " + yummy.value + " topping and a " + pizzaCrust.value + " crust" );
   alert(sizePrice[2] + yummyPrice[0] + crustPrice[0]);
 
   }
+
+  if (pizzaSize.value=="large" && yummy.value=="mushrooms" && pizzaCrust.value == "gluten-free"){
+    alert("You placed an order for a " + pizzaSize.value + " pizza with " + yummy.value + " toppings and a " + pizzaCrust.value + " crust" );
+  alert(sizePrice[2] + yummyPrice[0] + crustPrice[1]);
+
+  }
+
+  if (pizzaSize.value=="large" && yummy.value=="mushrooms" && pizzaCrust.value == "thick-crust"){
+    alert("you placed an order for a " + pizzaSize.value + " pizza with " + yummy.value + " toppings and a " + pizzaCrust.value + " crust" );
+  alert(sizePrice[2] + yummyPrice[0] + crustPrice[2]);
+
+  }
+
+  if (pizzaSize.value=="medium" && yummy.value=="mushrooms" && pizzaCrust.value == "crispy"){
+    alert("you placed an order for a " + pizzaSize.value + " pizza with a " + yummy.value + " topping and a " + pizzaCrust.value + " crust" );
+  alert(sizePrice[1] + yummyPrice[0] + crustPrice[0]);
+
+  }
+
+  if (pizzaSize.value=="medium" && yummy.value=="mushrooms" && pizzaCrust.value == "gluten-free"){
+    alert("you placed an order for a " + pizzaSize.value + " pizza with a " + yummy.value + " topping and a " + pizzaCrust.value + " crust" );
+  alert(sizePrice[1] + yummyPrice[0] + crustPrice[1]);
+
+  }
+
+  if (pizzaSize.value=="medium" && yummy.value=="mushrooms" && pizzaCrust.value == "thick-crust"){
+    alert("you placed an order for a " + pizzaSize.value + " pizza with a " + yummy.value + " topping and a " + pizzaCrust.value + " crust" );
+  alert(sizePrice[1] + yummyPrice[0] + crustPrice[2]);
+
+  }
+
+  if (pizzaSize.value=="small" && yummy.value=="mushrooms" && pizzaCrust.value == "crispy"){
+    alert("you placed an order for a " + pizzaSize.value + " pizza with a " + yummy.value + " topping and a " + pizzaCrust.value + " crust" );
+  alert(sizePrice[0] + yummyPrice[0] + crustPrice[0]);
+
+  }
+
+  if (pizzaSize.value=="small" && yummy.value=="mushrooms" && pizzaCrust.value == "gluten-free"){
+    alert("you placed an order for a " + pizzaSize.value + " pizza with a " + yummy.value + " topping and a " + pizzaCrust.value + " crust" );
+  alert(sizePrice[0] + yummyPrice[0] + crustPrice[1]);
+
+  }
+
+  if (pizzaSize.value=="small" && yummy.value=="mushrooms" && pizzaCrust.value == "thick-crust"){
+    alert("you placed an order for a " + pizzaSize.value + " pizza with a " + yummy.value + " topping and a " + pizzaCrust.value + " crust" );
+  alert(sizePrice[0] + yummyPrice[0] + crustPrice[2]);
+
+  }
+
+  // pineapple
+
+  if (pizzaSize.value=="large" && yummy.value=="pineapple" && pizzaCrust.value == "crispy"){
+    alert("you placed an order for a " + pizzaSize.value + " pizza with a " + yummy.value + " topping and a " + pizzaCrust.value + " crust" );
+  alert(sizePrice[2] + yummyPrice[1] + crustPrice[0]);
+
+  }
+
+  if (pizzaSize.value=="large" && yummy.value=="pineapple" && pizzaCrust.value == "gluten-free"){
+    alert("You placed an order for a " + pizzaSize.value + " pizza with " + yummy.value + " toppings and a " + pizzaCrust.value + " crust" );
+  alert(sizePrice[2] + yummyPrice[1] + crustPrice[1]);
+
+  }
+
+  if (pizzaSize.value=="large" && yummy.value=="pineapple" && pizzaCrust.value == "thick-crust"){
+    alert("you placed an order for a " + pizzaSize.value + " pizza with " + yummy.value + " toppings and a " + pizzaCrust.value + " crust" );
+  alert(sizePrice[2] + yummyPrice[1] + crustPrice[2]);
+
+  }
+
+  if (pizzaSize.value=="medium" && yummy.value=="pineapple" && pizzaCrust.value == "crispy"){
+    alert("you placed an order for a " + pizzaSize.value + " pizza with a " + yummy.value + " topping and a " + pizzaCrust.value + " crust" );
+  alert(sizePrice[1] + yummyPrice[1] + crustPrice[0]);
+
+  }
+
+  if (pizzaSize.value=="medium" && yummy.value=="pineapple" && pizzaCrust.value == "gluten-free"){
+    alert("you placed an order for a " + pizzaSize.value + " pizza with a " + yummy.value + " topping and a " + pizzaCrust.value + " crust" );
+  alert(sizePrice[1] + yummyPrice[1] + crustPrice[1]);
+
+  }
+
+  if (pizzaSize.value=="medium" && yummy.value=="pineapple" && pizzaCrust.value == "thick-crust"){
+    alert("you placed an order for a " + pizzaSize.value + " pizza with a " + yummy.value + " topping and a " + pizzaCrust.value + " crust" );
+  alert(sizePrice[1] + yummyPrice[1] + crustPrice[2]);
+
+  }
+
+  if (pizzaSize.value=="small" && yummy.value=="pineapple" && pizzaCrust.value == "crispy"){
+    alert("you placed an order for a " + pizzaSize.value + " pizza with a " + yummy.value + " topping and a " + pizzaCrust.value + " crust" );
+  alert(sizePrice[0] + yummyPrice[1] + crustPrice[0]);
+
+  }
+
+  if (pizzaSize.value=="small" && yummy.value=="pineapple" && pizzaCrust.value == "gluten-free"){
+    alert("you placed an order for a " + pizzaSize.value + " pizza with a " + yummy.value + " topping and a " + pizzaCrust.value + " crust" );
+  alert(sizePrice[0] + yummyPrice[1] + crustPrice[1]);
+
+  }
+
+  if (pizzaSize.value=="small" && yummy.value=="pineapple" && pizzaCrust.value == "thick-crust"){
+    alert("you placed an order for a " + pizzaSize.value + " pizza with a " + yummy.value + " topping and a " + pizzaCrust.value + " crust" );
+  alert(sizePrice[0] + yummyPrice[1] + crustPrice[2]);
+
+  }
+
+  //  ham
+  if (pizzaSize.value=="large" && yummy.value=="ham" && pizzaCrust.value == "crispy"){
+    alert("you placed an order for a " + pizzaSize.value + " pizza with a " + yummy.value + " topping and a " + pizzaCrust.value + " crust" );
+  alert(sizePrice[2] + yummyPrice[2] + crustPrice[0]);
+
+  }
+
+  if (pizzaSize.value=="large" && yummy.value=="ham" && pizzaCrust.value == "gluten-free"){
+    alert("You placed an order for a " + pizzaSize.value + " pizza with " + yummy.value + " toppings and a " + pizzaCrust.value + " crust" );
+  alert(sizePrice[2] + yummyPrice[1] + crustPrice[1]);
+
+  }
+
+  if (pizzaSize.value=="large" && yummy.value=="pineapple" && pizzaCrust.value == "thick-crust"){
+    alert("you placed an order for a " + pizzaSize.value + " pizza with " + yummy.value + " toppings and a " + pizzaCrust.value + " crust" );
+  alert(sizePrice[2] + yummyPrice[2] + crustPrice[2]);
+
+  }
+
+  if (pizzaSize.value=="medium" && yummy.value=="ham" && pizzaCrust.value == "crispy"){
+    alert("you placed an order for a " + pizzaSize.value + " pizza with a " + yummy.value + " topping and a " + pizzaCrust.value + " crust" );
+  alert(sizePrice[1] + yummyPrice[2] + crustPrice[0]);
+
+  }
+
+  if (pizzaSize.value=="medium" && yummy.value=="ham" && pizzaCrust.value == "gluten-free"){
+    alert("you placed an order for a " + pizzaSize.value + " pizza with a " + yummy.value + " topping and a " + pizzaCrust.value + " crust" );
+  alert(sizePrice[1] + yummyPrice[2] + crustPrice[1]);
+
+  }
+
+  if (pizzaSize.value=="medium" && yummy.value=="ham" && pizzaCrust.value == "thick-crust"){
+    alert("you placed an order for a " + pizzaSize.value + " pizza with a " + yummy.value + " topping and a " + pizzaCrust.value + " crust" );
+  alert(sizePrice[1] + yummyPrice[2] + crustPrice[2]);
+
+  }
+
+  if (pizzaSize.value=="small" && yummy.value=="ham" && pizzaCrust.value == "crispy"){
+    alert("you placed an order for a " + pizzaSize.value + " pizza with a " + yummy.value + " topping and a " + pizzaCrust.value + " crust" );
+  alert(sizePrice[0] + yummyPrice[2] + crustPrice[0]);
+
+  }
+
+  if (pizzaSize.value=="small" && yummy.value=="ham" && pizzaCrust.value == "gluten-free"){
+    alert("you placed an order for a " + pizzaSize.value + " pizza with a " + yummy.value + " topping and a " + pizzaCrust.value + " crust" );
+  alert(sizePrice[0] + yummyPrice[2] + crustPrice[1]);
+
+  }
+
+  if (pizzaSize.value=="small" && yummy.value=="ham" && pizzaCrust.value == "thick-crust"){
+    alert("you placed an order for a " + pizzaSize.value + " pizza with a " + yummy.value + " topping and a " + pizzaCrust.value + " crust" );
+  alert(sizePrice[0] + yummyPrice[2] + crustPrice[2]);
+
+  }
+
+
+
 }
-
-
-  // let size = document.getElementById("size");
-  // let crust = document.getElementById("crust").value;
-  // let toppings = [];
-  // let checkboxes = document.getElementByName("toppings").checked;
-  // let large = document.getElementById("large");
-  
  
   
-    // (let  == checkboxes.length;) {
-    // if (checkboxes[i].checked) {
-    //   toppings.push(checkboxes[i].value);
-
-    // }
-
-
-
-
-// let total = ("size" + "crust" + "toppings"){
-//   let pizzaSizes = ("small", "medium", "large");
-//   let sizePrice = (300, 500, 700);
-//   let crust = ("crispy", "gluten-free", "classic-thin", "classic-thick");
-//   let crustPrice = (150, 150, 150, 150);
-//   let toppings = ("Sausage and Mushrooms ", "Pepperoni and Bacon",
-//   "Ham and Pineapple", "Sausage and Pepper");
-// let toppingsPrice = (150, 150, 150, 150 );
-// }
-// let pizzaSizes =["small", "medium", "large"];
-
-
-
-
-
-
-// let pizzaTotal = (size + crust + (toppings).checked + (delivery).checked).value;
-// return true;
-// alert("Your total price is 'pizzaTotal'");
-
-// // mushroom
-// if (size.value == "large" && crust.value == "crispy" && toppings.value == "Mushrooms") {
-//   let pizzaTotal = 700 + 150 + 150;
-//   alert ("Your total price is 'pizzaTotal'")
-
-// if (size.value == "medium" && crust.value == "crispy" && toppings.value == "Mushrooms") {
-//   let pizzaTotal = 700 + 150 + 150;
-//   alert ("Your total price is 'pizzaTotal'")
-
-// if (size.value == "small" && crust.value == "crispy" && toppings.value == "Mushrooms") {
-//   let pizzaTotal = 700 + 150 + 150;
-//   alert ("Your total price is 'pizzaTotal'")
-
-// if (size.value == "large" && crust.value == "gluten-free" && toppings.value == "Mushrooms") {
-//   let pizzaTotal = 700 + 150 + 150;
-
-// if (size.value == "medium" && crust.value == "gluten-free" && toppings.value == "Mushrooms") {
-//   let pizzaTotal = 700 + 150 + 150;
-//   alert ("Your total price is pizzaTotal")
-
-// if (size.value == "small" && crust.value == "gluten-free" && toppings.value == "Mushrooms") {
-//   let pizzaTotal = 700 + 150 + 150;
-//   alert ("Your total price is pizzaTotal")
-
-// if (size.value == "large" && crust.value == "thick-crust" && toppings.value == "Mushrooms") {
-//   let pizzaTotal = 700 + 150 + 150;
-//   alert ("Your total price is pizzaTotal")
-
-// if (size.value == "medium" && crust.value == "thick-crust" && toppings.value == "Mushrooms") {
-//   let pizzaTotal = 700 + 150 + 150;
-//   alert ("Your total price is pizzaTotal")
-
-// if (size.value == "small" && crust.value == "thick-crust" && toppings.value == "Mushrooms") {
-//   let total = 700 + 150 + 150;
-//   alert ("Your total price is pizzaTotal")
-
-//   // pepperoni
-
-//   if (size.value == "large" && crust.value == "crispy" && toppings.value == "pepperoni") {
-//     let total = 700 + 150 + 150;
-//     alert ("Your total price is pizzaTotal")
-  
-// if (size.value == "medium" && crust.value == "crispy" && toppings.value == "pepperoni") {
-//   let total = 700 + 150 + 150;
-//   alert ("Your total price is pizzaTotal")
-    
-// if (size.value == "small" && crust.value == "crispy" && toppings.value == "pepperoni") {
-//   let total = 700 + 150 + 150;
-//   alert ("Your total price is pizzaTotal")
-  
-// if (size.value == "large" && crust.value == "gluten-free" && toppings.value == "pepperoni") {
-//   let total = 700 + 150 + 150;
-//   alert ("Your total price is pizzaTotal")
-  
-// if (size.value == "medium" && crust.value == "gluten-free" && toppings.value == "pepperoni") {
-//   let total = 700 + 150 + 150;
-//   alert ("Your total price is total")
-  
-// if (size.value == "small" && crust.value == "gluten-free" && toppings.value == "pepperoni") {
-//   let total = 700 + 150 + 150;
-//   alert ("Your total price is pizzaTotal")
-  
-// if (size.value == "large" && crust.value == "thick-crust" && toppings.value == "pepperoni") {
-//   let total = 700 + 150 + 150;
-//   alert ("Your total price is pizzaTotal")
-  
-// if (size.value == "medium" && crust.value == "thick-crust" && toppings.value == "pepperoni") {
-//   let total = 700 + 150 + 150;
-//   alert ("Your total price is pizzaTotal")
-  
-// if (size.value == "small" && crust.value == "thick-crust" && toppings.value == "pepperoni") {
-//   let total = 700 + 150 + 150;
-//   alert ("Your total price is pizzaTotal")
-
-//     // ham
-// if (size.value == "large" && crust.value == "crispy" && toppings.value == "ham") {
-//   let total = 700 + 150 + 150;
-//   alert ("Your total price is pizzaTotal")
-    
-// if (size.value == "medium" && crust.value == "crispy" && toppings.value == "ham") {
-//   let total = 700 + 150 + 150;
-//   alert ("Your total price is pizzaTotal")
-      
-// if (size.value == "small" && crust.value == "crispy" && toppings.value == "ham") {
-//   let total = 700 + 150 + 150;
-//   alert ("Your total price is pizzaTotal")
-    
-// if (size.value == "large" && crust.value == "gluten-free" && toppings.value == "ham") {
-//   let total = 700 + 150 + 150;
-//   alert ("Your total price is pizzaTotal")
-    
-// if (size.value == "medium" && crust.value == "gluten-free" && toppings.value == "ham") {
-//   let total = 700 + 150 + 150;
-//   alert ("Your total price is pizzaTotal")
-    
-// if (size.value == "small" && crust.value == "gluten-free" && toppings.value == "ham") {
-//   let total = 700 + 150 + 150;
-//   alert ("Your total price is pizzaTotal")
-    
-// if (size.value == "large" && crust.value == "thick-crust" && toppings.value == "ham") {
-//   let total = 700 + 150 + 150;
-//   alert ("Your total price is pizzaTotal")
-    
-// if (size.value == "medium" && crust.value == "thick-crust" && toppings.value == "ham") {
-//   let total = 700 + 150 + 150;
-//   alert ("Your total price is pizzaTotal")
-    
-// if (size.value == "small" && crust.value == "thick-crust" && toppings.value == "ham") {
-//   let total = 700 + 150 + 150;
-//   alert ("Your total price is pizzaTotal")
-
-//       // pineapple
-// if (size.value == "large" && crust.value == "crispy" && toppings.value == "pineapple") {
-//   let total = 700 + 150 + 150;
-//   alert ("Your total price is pizzaTotal")
-      
-// if (size.value == "medium" && crust.value == "crispy" && toppings.value == "pineapple") {
-//   let total = 700 + 150 + 150;
-//   alert ("Your total price is pizzaTotal")
-        
-// if (size.value == "small" && crust.value == "crispy" && toppings.value == "pineapple") {
-//   let total = 700 + 150 + 150;
-//   alert ("Your total price is pizzaTotal")
-      
-// if (size.value == "large" && crust.value == "gluten-free" && toppings.value == "pineapple") {
-//   let total = 700 + 150 + 150;
-//   alert ("Your total price is pizzaTotal")
-      
-// if (size.value == "medium" && crust.value == "gluten-free" && toppings.value == "pineapple") {
-//   let total = 700 + 150 + 150;
-//   alert ("Your total price is pizzaTotal")
-      
-// if (size.value == "small" && crust.value == "gluten-free" && toppings.value == "pineapple") {
-//   let total = 700 + 150 + 150;
-//   alert ("Your total price is pizzaTotal")
-      
-// if (size.value == "large" && crust.value == "thick-crust" && toppings.value == "pineapple") {
-//   let total = 700 + 150 + 150;
-//   alert ("Your total price is pizzaTotal")
-      
-// if (size.value == "medium" && crust.value == "thick-crust" && toppings.value == "pineapple") {
-//   let total = 700 + 150 + 150;
-//   alert ("Your total price is pizzaTotal")
-      
-// if (size.value == "small" && crust.value == "thick-crust" && toppings.value == "pineapple") {
-//   let total = 700 + 150 + 150;
-//   alert ("Your total price is total")
-// }
-  
-
-  
-       
 
